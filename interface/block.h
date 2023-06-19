da1560c1908f155be69ff7d13156e378d47b4ea0
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-06-19 18:51:50 -0500

    include cbp

--- interface/block.h
+++ interface/block.h
@@ -1,9 +1,27 @@
 #ifndef BLOCK_GUI_H
 #define BLOCK_GUI_H
 
+#include <SFML/Graphics.hpp>
+#include "../structures/doubleList.h"
+#include "button.h"
+
+using namespace sf;
+
 class SFMLBlock {
+    RectangleShape* boxBlock;
+    Image userProfile;
+
+public:
+    SFMLBlock(const std::string username, string metadataBlock);
+
+    // renderiza la metadada del bloque y la imagen del usuario
+    void viewBlock(RenderWindow& window, float pos_x, float pos_y);
+
+    // cambia de color del fondo de bloque
+    void changeColor(Color color);
 
 };
+
 
 
 
